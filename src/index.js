585bf0ffbe9e873ebd0808704a39de15512fbe30
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-10-18 20:26:36 +0300

    ADD props and PropTypes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
 const init = () => {
+  const settings = {
+    gameTime: 1,
+    errorAmount: 5
+  };
+
   ReactDOM.render(
-      <App />,
+      <App
+        gameTime = {settings.gameTime}
+        errorAmount = {settings.errorAmount}
+      />,
       document.querySelector(`#root`)
   );
 };
