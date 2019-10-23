00cda5487bbd14ff6862f8a1172dc5e2aa5b596c
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-10-24 00:38:14 +0300

    ADD mock data for questions

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,23 +3,48 @@ import PropTypes from "prop-types";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 
 const App = (props) => {
-  const {gameTime, errorAmount} = props;
+  const {gameTime, errorAmount, questions} = props;
 
-  const onGameStartButtonClick = () => {
+  const startButtonClickHandler = () => {
   };
 
   return (
     <WelcomeScreen
       time={gameTime}
-      errorAmount = {errorAmount}
-      onWelcomeButtonClick = {onGameStartButtonClick}
+      errorAmount={errorAmount}
+      onWelcomeButtonClick = {startButtonClickHandler}
     />
   );
 };
 
 App.propTypes = {
   gameTime: PropTypes.number.isRequired,
-  errorAmount: PropTypes.number.isRequired
+  errorAmount: PropTypes.number.isRequired,
+  questions: PropTypes.arrayOf(PropTypes.oneOfType([
+    PropTypes.exact({
+      type: PropTypes.oneOf([`genre`]),
+      genre: PropTypes.oneOf([`jazz`, `rock`, `pop`]),
+      answers: PropTypes.arrayOf(
+          PropTypes.exact({
+            src: PropTypes.string.isRequired,
+            genre: PropTypes.oneOf([`jazz`, `rock`, `pop`])
+          })
+      )
+    }),
+    PropTypes.exact({
+      type: PropTypes.oneOf([`artist`]),
+      song: PropTypes.exact({
+        artist: PropTypes.string.isRequired,
+        src: PropTypes.string.isRequired
+      }),
+      answers: PropTypes.arrayOf(
+          PropTypes.exact({
+            artist: PropTypes.string.isRequired,
+            image: PropTypes.string.isRequired
+          })
+      )
+    })
+  ]))
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
+import {questions} from "./mocks/questions.js";
 
 const init = () => {
   const settings = {
@@ -12,6 +13,7 @@ const init = () => {
       <App
         gameTime={settings.gameTime}
         errorAmount={settings.errorAmount}
+        questions={questions}
       />,
       document.querySelector(`#root`)
   );
