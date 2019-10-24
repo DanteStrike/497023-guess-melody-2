a474b7b4a763e64686a9efb26aacaa0e23e8126c
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-10-25 00:54:35 +0300

    Update app.test.js

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -3,14 +3,61 @@ import renderer from "react-test-renderer";
 import App from "./app.jsx";
 
 it(`render correctly App component`, () => {
-  const AppComponent = renderer
+  const questionsMock = [
+    {
+      type: `genre`,
+      genre: `pop`,
+      answers: [
+        {
+          src: ``,
+          genre: `rock`
+        },
+        {
+          src: ``,
+          genre: `jazz`
+        },
+        {
+          src: ``,
+          genre: `pop`
+        },
+        {
+          src: ``,
+          genre: `pop`
+        }
+      ]
+    },
+    {
+      type: `artist`,
+      song: {
+        artist: `Plаcido Domingo`,
+        src: ``
+      },
+      answers: [
+        {
+          artist: `Luciano Pavarotti`,
+          image: ``
+        },
+        {
+          artist: `Plаcido Domingo`,
+          image: ``
+        },
+        {
+          artist: `Jose Carreras`,
+          image: ``
+        }
+      ]
+    }
+  ]
+
+  const snapshot = renderer
     .create(
         <App
           gameTime={1}
           errorAmount={1}
+          questions={questionsMock}
         />
     )
     .toJSON();
 
-  expect(AppComponent).toMatchSnapshot();
+  expect(snapshot).toMatchSnapshot();
 });
