3146329719ac97115f07951e57182a45ce063f20
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-10-24 08:23:10 +0300

    ADD more mocks questions

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -42,47 +42,47 @@ export const questions = [
       }
     ]
   },
-  // {
-  //   type: `genre`,
-  //   genre: `pop`,
-  //   answers: [
-  //     {
-  //       src: `https://upload.wikimedia.org/wikipedia/commons/d/de/United_States_Navy_Band_-_God_Save_the_Queen.ogg`,
-  //       genre: `jazz`
-  //     },
-  //     {
-  //       src: `https://upload.wikimedia.org/wikipedia/commons/d/de/United_States_Navy_Band_-_God_Save_the_Queen.ogg`,
-  //       genre: `jazz`
-  //     },
-  //     {
-  //       src: `https://upload.wikimedia.org/wikipedia/commons/d/de/United_States_Navy_Band_-_God_Save_the_Queen.ogg`,
-  //       genre: `pop`
-  //     },
-  //     {
-  //       src: `https://upload.wikimedia.org/wikipedia/commons/d/de/United_States_Navy_Band_-_God_Save_the_Queen.ogg`,
-  //       genre: `rock`
-  //     }
-  //   ]
-  // },
-  // {
-  //   type: `artist`,
-  //   song: {
-  //     artist: `Jose Carreras`,
-  //     src: `https://upload.wikimedia.org/wikipedia/commons/d/de/United_States_Navy_Band_-_God_Save_the_Queen.ogg`
-  //   },
-  //   answers: [
-  //     {
-  //       artist: `Luciano Pavarotti`,
-  //       image: `http://placehold.it/134x134`
-  //     },
-  //     {
-  //       artist: `Plаcido Domingo`,
-  //       image: `http://placehold.it/134x134`
-  //     },
-  //     {
-  //       artist: `Jose Carreras`,
-  //       image: `http://placehold.it/134x134`
-  //     }
-  //   ]
-  // },
+  {
+    type: `genre`,
+    genre: `pop`,
+    answers: [
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/d/de/United_States_Navy_Band_-_God_Save_the_Queen.ogg`,
+        genre: `jazz`
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/d/de/United_States_Navy_Band_-_God_Save_the_Queen.ogg`,
+        genre: `jazz`
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/d/de/United_States_Navy_Band_-_God_Save_the_Queen.ogg`,
+        genre: `pop`
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/d/de/United_States_Navy_Band_-_God_Save_the_Queen.ogg`,
+        genre: `rock`
+      }
+    ]
+  },
+  {
+    type: `artist`,
+    song: {
+      artist: `Jose Carreras`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/d/de/United_States_Navy_Band_-_God_Save_the_Queen.ogg`
+    },
+    answers: [
+      {
+        artist: `Luciano Pavarotti`,
+        image: `http://placehold.it/134x134`
+      },
+      {
+        artist: `Plаcido Domingo`,
+        image: `http://placehold.it/134x134`
+      },
+      {
+        artist: `Jose Carreras`,
+        image: `http://placehold.it/134x134`
+      }
+    ]
+  }
 ];
