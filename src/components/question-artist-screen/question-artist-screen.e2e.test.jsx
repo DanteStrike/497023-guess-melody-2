9b396338c6daf2a0ca25576f88bbc864dfe89b45
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-10-26 01:06:56 +0300

    ADD tests for ques return answers type

---
+++ src/components/question-artist-screen/question-artist-screen.e2e.test.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Enzyme, {shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import QuestionArtistScreen from "./question-artist-screen.jsx";
+
+Enzyme.configure({adapter: new Adapter()});
+
+it(`Should return user answers correctly on submit form`, () => {
+  const questionMock = {
+    song: {
+      artist: `Plаcido Domingo`,
+      src: ``
+    },
+    answers: [
+      {
+        artist: `Plаcido Domingo`,
+        image: ``
+      },
+    ]
+  };
+
+  const onAnswerClickMock = jest.fn();
+
+  const component = shallow(
+      <QuestionArtistScreen
+        screenIndex={0}
+        question={questionMock}
+        onAnswerClick={onAnswerClickMock}
+      />
+  );
+
+  const preventDefault = () => {};
+
+  const artistQuestionForm = component.find(`form.game__artist`);
+  artistQuestionForm.simulate(`change`, {preventDefault});
+  expect(onAnswerClickMock.mock.calls[0][0]).toMatchObject([]);
+});
--- src/components/question-genre-screen/question-genre-screen.e2e.test.jsx
+++ src/components/question-genre-screen/question-genre-screen.e2e.test.jsx
@@ -5,27 +5,15 @@ import QuestionGenreScreen from "./question-genre-screen.jsx";
 
 Enzyme.configure({adapter: new Adapter()});
 
-it(`should preventDefault if click submit button`, () => {
+it(`Should preventDefault on form submit`, () => {
   const questionMock = {
     type: `genre`,
     genre: `jazz`,
     answers: [
       {
         src: ``,
-        genre: `rock`
-      },
-      {
-        src: ``,
-        genre: `rock`
-      },
-      {
-        src: ``,
-        genre: `pop`
-      },
-      {
-        src: ``,
         genre: `jazz`
-      },
+      }
     ]
   };
 
@@ -39,8 +27,41 @@ it(`should preventDefault if click submit button`, () => {
       />
   );
 
-  const componentForm = component.find(`form`);
+  const componentForm = component.find(`form.game__tracks`);
   componentForm.simulate(`submit`, {preventDefault});
 
   expect(preventDefault).toBeCalled();
+});
+
+it(`Should return user answers correctly on submit form`, () => {
+  const questionMock = {
+    type: `genre`,
+    genre: `jazz`,
+    answers: [
+      {
+        src: ``,
+        genre: `jazz`
+      },
+      {
+        src: ``,
+        genre: `pop`
+      }
+    ]
+  };
+
+  const onAnswerClickMock = jest.fn();
+
+  const component = shallow(
+      <QuestionGenreScreen
+        screenIndex={0}
+        question={questionMock}
+        onAnswerClick={onAnswerClickMock}
+      />
+  );
+
+  const preventDefault = () => {};
+
+  const genreQuestionForm = component.find(`form.game__tracks`);
+  genreQuestionForm.simulate(`submit`, {preventDefault});
+  expect(onAnswerClickMock.mock.calls[0][0]).toMatchObject([]);
 });
