6677d95ee028e25fb0641ad5ca513b5ca117d610
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-10-26 01:04:10 +0300

    REFACTOR App e2e test

--- src/components/app/app.e2e.test.js
+++ src/components/app/app.e2e.test.js
@@ -5,85 +5,127 @@ import App from "./app.jsx";
 
 Enzyme.configure({adapter: new Adapter()});
 
-it(`APP screens should switch`, () => {
-  const questionsMock = [
-    {
-      type: `genre`,
-      genre: `jazz`,
-      answers: [
-        {
-          src: ``,
-          genre: `rock`
-        },
-        {
-          src: ``,
-          genre: `jazz`
-        },
-        {
-          src: ``,
-          genre: `pop`
-        },
-        {
-          src: ``,
-          genre: `jazz`
-        },
-      ]
-    },
-    {
-      type: `artist`,
-      song: {
-        artist: `Plаcido Domingo`,
-        src: ``
+describe(`APP screens should switch`, () => {
+
+  it(`On start game button click: welcome screen should switch to first question`, () => {
+    const questionsMock = [
+      {
+        type: `genre`,
+        genre: `jazz`,
+        answers: [
+          {
+            src: ``,
+            genre: `jazz`
+          },
+        ]
       },
-      answers: [
-        {
-          artist: `Jose Carreras`,
-          image: ``
-        },
-        {
-          artist: `Luciano Pavarotti`,
-          image: ``
-        },
-        {
+    ];
+
+    const AppComponent = mount(
+        <App
+          gameTime={1}
+          errorAmount={1}
+          questions={questionsMock}
+        />
+    );
+
+    expect(AppComponent.state(`questionIndex`)).toEqual(-1);
+
+    const startGameButton = AppComponent.find(`.welcome__button`);
+    expect(startGameButton.exists()).toEqual(true);
+    startGameButton.simulate(`click`);
+
+    expect(AppComponent.state(`questionIndex`)).toEqual(0);
+    AppComponent.update();
+
+    const questionGenreScreen = AppComponent.find(`.game--genre`);
+    expect(questionGenreScreen.exists()).toEqual(true);
+  });
+
+  it(`Should switch questions screens`, () => {
+    const questionsMock = [
+      {
+        type: `genre`,
+        genre: `jazz`,
+        answers: [
+          {
+            src: ``,
+            genre: `jazz`
+          },
+        ]
+      },
+      {
+        type: `artist`,
+        song: {
           artist: `Plаcido Domingo`,
-          image: ``
-        }
-      ]
-    }
-  ];
+          src: ``
+        },
+        answers: [
+          {
+            artist: `Plаcido Domingo`,
+            image: ``
+          }
+        ]
+      }
+    ];
 
-  const AppComponent = mount(
-      <App
-        gameTime={1}
-        errorAmount={1}
-        questions={questionsMock}
-      />
-  );
-  expect(AppComponent.state(`questionIndex`)).toEqual(-1);
+    const AppComponent = mount(
+        <App
+          gameTime={1}
+          errorAmount={1}
+          questions={questionsMock}
+        />
+    );
 
-  const startGameButton = AppComponent.find(`.welcome__button`);
-  expect(startGameButton.exists()).toEqual(true);
-  startGameButton.simulate(`click`);
+    AppComponent.setState({questionIndex: 0});
+    const genreScreenForm = AppComponent.find(`.game__tracks`);
+    expect(genreScreenForm.exists()).toEqual(true);
+    genreScreenForm.simulate(`submit`, {
+      preventDefault: ()=>{}
+    });
 
-  expect(AppComponent.state(`questionIndex`)).toEqual(0);
-  AppComponent.update();
+    expect(AppComponent.state(`questionIndex`)).toEqual(1);
+    AppComponent.update();
 
-  const questionGenreScreen = AppComponent.find(`.game--genre`);
-  expect(questionGenreScreen.exists()).toEqual(true);
+    const questionArtistScreen = AppComponent.find(`.game--artist`);
+    expect(questionArtistScreen.exists()).toEqual(true);
+  });
 
-  const genreForm = AppComponent.find(`.game__tracks`);
-  expect(genreForm.exists()).toEqual(true);
-  genreForm.simulate(`submit`, {preventDefault: ()=>{}});
+  it(`Should return to welcome screen after game end`, () => {
+    const questionsMock = [
+      {
+        type: `artist`,
+        song: {
+          artist: `Plаcido Domingo`,
+          src: ``
+        },
+        answers: [
+          {
+            artist: `Plаcido Domingo`,
+            image: ``
+          }
+        ]
+      },
+    ];
 
-  expect(AppComponent.state(`questionIndex`)).toEqual(1);
-  AppComponent.update();
+    const AppComponent = mount(
+        <App
+          gameTime={1}
+          errorAmount={1}
+          questions={questionsMock}
+        />
+    );
 
-  const questionArtistScreen = AppComponent.find(`.game--artist`);
-  expect(questionArtistScreen.exists()).toEqual(true);
+    AppComponent.setState({questionIndex: 0});
 
-  const artistForm = AppComponent.find(`.game__artist`);
-  expect(artistForm.exists()).toEqual(true);
-  artistForm.simulate(`change`);
+    const artistScreenForm = AppComponent.find(`.game__artist`);
+    expect(artistScreenForm.exists()).toEqual(true);
+    artistScreenForm.simulate(`change`);
 
-  expect(AppComponent.state(`questionIndex`)).toEqual(-1);
+    expect(AppComponent.state(`questionIndex`)).toEqual(-1);
+    AppComponent.update();
+
+    const startGameButton = AppComponent.find(`.welcome__button`);
+    expect(startGameButton.exists()).toEqual(true);
+  });
 });
