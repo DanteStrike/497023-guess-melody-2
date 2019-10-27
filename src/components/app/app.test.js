cd6bdc211f1cd2f45a648a617c366c60dbea9613
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-10-27 21:53:24 +0300

    Update app.test.js

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,60 +1,15 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import {questions} from "../../mocks/questions";
 import App from "./app.jsx";
 
 it(`render correctly App component`, () => {
-  const questionsMock = [
-    {
-      type: `genre`,
-      genre: `pop`,
-      answers: [
-        {
-          src: ``,
-          genre: `rock`
-        },
-        {
-          src: ``,
-          genre: `jazz`
-        },
-        {
-          src: ``,
-          genre: `pop`
-        },
-        {
-          src: ``,
-          genre: `pop`
-        }
-      ]
-    },
-    {
-      type: `artist`,
-      song: {
-        artist: `Plаcido Domingo`,
-        src: ``
-      },
-      answers: [
-        {
-          artist: `Luciano Pavarotti`,
-          image: ``
-        },
-        {
-          artist: `Plаcido Domingo`,
-          image: ``
-        },
-        {
-          artist: `Jose Carreras`,
-          image: ``
-        }
-      ]
-    }
-  ];
-
   const snapshot = renderer
     .create(
         <App
           gameTime={1}
           errorAmount={1}
-          questions={questionsMock}
+          questions={questions}
         />
     )
     .toJSON();
