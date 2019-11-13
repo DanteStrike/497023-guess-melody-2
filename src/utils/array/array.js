ad373f84515a57f4a65fb4a3443dc0bce0027db3
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-11-13 18:18:53 +0300

    DISABLE utils funcs

--- src/utils/array/array.js
+++ src/utils/array/array.js
@@ -1,3 +1,16 @@
 export const newArray = (length = 0, fillValue = null) => {
   return new Array(length).fill(fillValue);
 };
+
+// export const updateItemInArray = (array, itemId, updateItemCallback) => {
+//   const updatedItems = array.map((item) => {
+//     if (item.id !== itemId) {
+//       return item;
+//     }
+
+//     const updatedItem = updateItemCallback(item);
+//     return updatedItem;
+//   });
+
+//   return updatedItems;
+// };
